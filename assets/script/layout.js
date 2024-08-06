dbaf0c0a1ac5f841b2c9cf5ab9e2f3836587de7d
David Jansen <jansend33@gmail.com>
2024-08-07 00:11:59 +0700

    moved head to header.html to make it dynamic

--- assets/script/layout.js
+++ assets/script/layout.js
@@ -13,3 +13,11 @@ document.addEventListener("DOMContentLoaded", function() {
       document.getElementById('footer').innerHTML = data;
     });
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+  fetch('pages/header.html')
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById('header').innerHTML = data;
+    });
+});
