816b604b22dbcc779c75a94824357531876bf4d6
David Jansen <jansend33@gmail.com>
2024-08-08 21:18:09 +0700

    Finish Navbar

--- assets/script/layout.js
+++ assets/script/layout.js
@@ -21,3 +21,24 @@ document.addEventListener("DOMContentLoaded", function() {
       document.getElementById('header').innerHTML = data;
     });
 });
+
+
+// document.addEventListener("DOMContentLoaded", function() {
+//   fetch('pages/nav.html')
+//     .then(response => response.text())
+//     .then(data => {
+//       document.getElementById('navbar').innerHTML = data;
+//     });
+
+//   fetch('pages/footer.html')
+//     .then(response => response.text())
+//     .then(data => {
+//       document.getElementById('footer').innerHTML = data;
+//     });
+
+//   fetch('pages/header.html')
+//     .then(response => response.text())
+//     .then(data => {
+//       document.getElementById('header').innerHTML = data;
+//     });
+// });
