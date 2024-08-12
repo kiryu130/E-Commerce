8d46a1b1e04c444cd4a5c2c9fc5ee22cbb7a7069
David Jansen <jansend33@gmail.com>
2024-08-12 13:05:09 +0700

    Add Dynamic Path Rules

--- assets/script/layout.js
+++ assets/script/layout.js
@@ -1,44 +1,47 @@
-document.addEventListener("DOMContentLoaded", function() {
-  fetch('pages/nav.html')
-    .then(response => response.text())
-    .then(data => {
-      document.getElementById('navbar').innerHTML = data;
-    });
-});
-
-document.addEventListener("DOMContentLoaded", function() {
-  fetch('pages/footer.html')
-    .then(response => response.text())
-    .then(data => {
-      document.getElementById('footer').innerHTML = data;
-    });
-});
-
-document.addEventListener("DOMContentLoaded", function() {
-  fetch('pages/header.html')
-    .then(response => response.text())
-    .then(data => {
-      document.getElementById('header').innerHTML = data;
-    });
-});
-
-
 // document.addEventListener("DOMContentLoaded", function() {
-//   fetch('pages/nav.html')
+//   fetch('${basePath}pages/nav.html')
 //     .then(response => response.text())
 //     .then(data => {
 //       document.getElementById('navbar').innerHTML = data;
 //     });
+// });
 
-//   fetch('pages/footer.html')
+// document.addEventListener("DOMContentLoaded", function() {
+//   fetch('${basePath}pages/footer.html')
 //     .then(response => response.text())
 //     .then(data => {
 //       document.getElementById('footer').innerHTML = data;
 //     });
+// });
 
-//   fetch('pages/header.html')
+// document.addEventListener("DOMContentLoaded", function() {
+//   fetch('${basePath}pages/header.html')
 //     .then(response => response.text())
 //     .then(data => {
 //       document.getElementById('header').innerHTML = data;
 //     });
 // });
+
+const basePath = window.location.pathname.includes("/pages/") ? "../" : "";
+
+document.addEventListener("DOMContentLoaded", function () {
+  fetch(`${basePath}pages/nav.html`)
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("navbar").innerHTML = data;
+    });
+
+  fetch(`${basePath}pages/footer.html`)
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("footer").innerHTML = data;
+    });
+
+  fetch(`${basePath}pages/header.html`)
+    .then((response) => response.text())
+    .then((data) => {
+      // Adjust CSS paths
+      data = data.replace(/href="assets\/css/g, `href="${basePath}assets/css`);
+      document.getElementById("header").innerHTML = data;
+    });
+});
